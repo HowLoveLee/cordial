c2cace27194c00558cee8a2f5cbc8c48629c44d6
Alex <jimmyshiro876@gmail.com>
2024-10-31 16:01:29 -0700

    Learning is a process

--- cordialproject/settingsprod.py
+++ cordialproject/settingsprod.py
@@ -125,9 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]  # Wrapping it in a list
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"  # This is where Django will collect static files for production
 
 
 # Default primary key field type
