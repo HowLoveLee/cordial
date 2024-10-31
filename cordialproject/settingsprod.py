f535fb564c72cf679f4c9b669895a07aa43f46ed
Alex <jimmyshiro876@gmail.com>
2024-10-31 13:55:20 -0700

    new cordial droplet setup.

--- cordialproject/settingsprod.py
+++ cordialproject/settingsprod.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cordial-associates.com', 'www.cordial-associates.com']
+ALLOWED_HOSTS = ['cordial-associates.com', 'www.cordial-associates.com', 'localhost']
 
 
 # Application definition
