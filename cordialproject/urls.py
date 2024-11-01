45482cabfcf2ad79eae5e5a62ec9cd334d064791
Alex <jimmyshiro876@gmail.com>
2024-10-31 17:22:41 -0700

    404 Error fixing.

--- cordialproject/urls.py
+++ cordialproject/urls.py
@@ -1,5 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
+# cordialproject/urls.py
+
 from django.urls import path
 from django.contrib import admin
 from cordialapp import views
@@ -9,6 +9,3 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('admin/', admin.site.urls),
 ]
-
-
-               # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
