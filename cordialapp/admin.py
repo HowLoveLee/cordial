45de844d4120f2ce022369412bb622d261dbb9c8
Alex <jimmyshiro876@gmail.com>
2024-10-31 23:31:28 -0700

    Access to static,media,templates.

--- cordialapp/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-
-from cordialapp.models import Exam, Location
-
-# Register your models here.
-admin.site.register(Exam)
-
-admin.site.register(Location)
--- staticfiles/javascript/script.js
+++
@@ -1,9 +0,0 @@
-function passwordCheck() {
-    const username = document.getElementById('studentEmail');
-    const password = doucment.getElementById('studentPassword');
-
-    if (password === username.substring(0, 10)) {
-        console.log("hey");
-        preventDefault(e);
-    }
-}
