46a71d980556605b96e99b5ea91e05a497bcd389
Alex <jimmyshiro876@gmail.com>
2024-10-31 17:07:04 -0700

    404 Error fixing.

--- cordialproject/wsgi.py
+++ cordialproject/wsgi.py
@@ -11,6 +11,20 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cordialproject.settings')
+"""
+WSGI config for cordialproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cordialproject.settingsprod')
+
+application = get_wsgi_application()
 
 application = get_wsgi_application()
