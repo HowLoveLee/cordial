ad7de5d03bc539bfeefa3b8cadb0f01c39613db5
brendachau <brendxss@gmail.com>
2024-11-06 19:31:46 -0800

    Changed login

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,9 +1,31 @@
-function passwordCheck() {
-    const username = document.getElementById('studentEmail');
-    const password = doucment.getElementById('studentPassword');
+function login() {
+    const loginDiv = document.getElementById('login-form');
+    const registerDiv = document.getElementById('register-form');
+    const wrapper = document.getElementById('wrapper');
 
-    if (password === username.substring(0, 10)) {
-        console.log("hey");
-        preventDefault(e);
+    wrapper.style.height = "500px"
+    registerDiv.style.display = "none";
+    loginDiv.style.display = "block";
+}
+function createAccount() {
+    const loginDiv = document.getElementById('login-form');
+    const registerDiv = document.getElementById('register-form');
+    const wrapper = document.getElementById('wrapper');
+
+    wrapper.style.height = "650px";
+    registerDiv.style.display = "block";
+    loginDiv.style.display = "none";
+}
+
+/* password validator */
+function matchingPass() {
+    const repeatPass = document.getElementById('r-password2');
+
+    if (repeatPass.value != document.getElementById('r-password').value) {
+        repeatPass.setCustomValidity('Password does not match');
+    } else {
+        repeatPass.setCustomValidity('');
     }
 }
+
+document.getElementById('studentID').innerHTML = document.getElementById('studentPassword').value;
